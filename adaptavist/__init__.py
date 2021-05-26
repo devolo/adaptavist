d9df233d9c0bca47a0be611f6958d5bb986d5c70
Markus Bong <Markus.Bong@devolo.de>
2021-05-26 11:29:17 +0200

    fix version

--- adaptavist/__init__.py
+++ adaptavist/__init__.py
@@ -10,7 +10,7 @@ except ImportError:
     from importlib_metadata import PackageNotFoundError, version  # type: ignore[misc]
 
 try:
-    __version__ = version("devolo_home_control_api")
+    __version__ = version("adaptavist")
 except PackageNotFoundError:
     # package is not installed - e.g. pulled and run locally
     __version__ = "0.0.0"
